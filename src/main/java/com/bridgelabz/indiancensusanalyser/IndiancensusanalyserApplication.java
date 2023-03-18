a97adba57262ca7a7689df4256eb699b9d2c5049
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-03-18 13:34:13 +0530

    Add: first commit.

---
+++ src/main/java/com/bridgelabz/indiancensusanalyser/IndiancensusanalyserApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.indiancensusanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiancensusanalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndiancensusanalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/indiancensusanalyser/IndiancensusanalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.indiancensusanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiancensusanalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
